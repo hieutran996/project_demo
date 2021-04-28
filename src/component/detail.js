0777ea152d891bd81be86a39709d4f9e49a6620c
Hieutran <hieudev96@gmail.com>
2021-04-28 17:37:53 +0700

    update

--- src/component/detail.js
+++ src/component/detail.js
@@ -21,8 +21,8 @@ function Detail() {
 
 
     useEffect(() => {
-      let crrData = Data.find(element => element.id == slug)
-      setDataDetail(crrData)
+      // let crrData = Data.find(element => element.id === slug)
+      // setDataDetail(crrData)
     }, [])
 
 
@@ -38,47 +38,7 @@ function Detail() {
           </Breadcrumb>
           <div className="site-layout-background" style={{ padding: 24, minHeight: 380 }}>
             <div className="site-card-wrapper">
-              {
-                dataDetail 
-                && 
-                <Row gutter={26}>
-                  <Col sm={24} md={4} className="pb-4">
-                    <img alt="example" src={dataDetail.image} />
-                  </Col>
-                  <Col sm={24} md={8} className="pb-4">
-                      <h2>{dataDetail.name}</h2>
-                      <div className="pb-2">
-                        {Language[Store.getState().languages.language].select_color}
-                      </div>
-                      <div className="pb-2">
-                        <Radio.Group onChange={onChange} value={value}>
-                          <Radio value={1}>{Language[Store.getState().languages.language].green}</Radio>
-                          <Radio value={2}>{Language[Store.getState().languages.language].red}</Radio>
-                          <Radio value={3}>{Language[Store.getState().languages.language].blue}</Radio>
-                          <Radio value={4}>{Language[Store.getState().languages.language].orange}</Radio>
-                        </Radio.Group>
-                      </div>
-                      <div className="pb-2">
-                        {Language[Store.getState().languages.language].select_size}
-                      </div>
-                      <div className="pb-2">
-                      <Select defaultValue="M" style={{ width: 120 }}>
-                          <Option value="M">M</Option>
-                          <Option value="L">L</Option>
-                          <Option value="XL">XL</Option>
-                          <Option value="XXL">XXL</Option>
-                        </Select>
-                      </div>
-                      <div className="pb-2">
-                        {Language[Store.getState().languages.language].select_amount}
-                      </div>
-                      <div className="pb-2">
-                        <InputNumber min={1} max={10} defaultValue={3} style={{ width: 120 }}/>
-                      </div>
 
-                  </Col>
-                </Row>
-              }
 
             </div>
           </div>
